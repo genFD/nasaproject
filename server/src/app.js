378ebf8226dc352f79d889799907451ed5512d70
hermkan <hermann.kanga.dev@gmail.com>
2022-03-03 10:48:25 -0500

    prepare for deployment  heroku

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,13 @@ import api from './routes/api.js';
 const filename = fileURLToPath(import.meta.url);
 
 const dirn = path.dirname(filename);
-
+const dirname = path.resolve();
 const publicFolder = path.join(dirn, '..', 'public');
 const indexHtml = path.join(dirn, '..', 'public', 'index.html');
+
+const buildFolder = path.join(dirname, '/client/build');
+
+const IndexBuildFolder = path.resolve(dirname, 'client', 'build', 'index.html');
 
 const app = express();
 app.use(
@@ -26,8 +30,10 @@ app.use(express.json());
 app.use(express.static(publicFolder));
 app.use('/v1', api);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(indexHtml));
-});
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(buildFolder));
+  app.get('/*', (req, res) => {
+    res.sendFile(IndexBuildFolder);
+  });
+}
 export default app;
